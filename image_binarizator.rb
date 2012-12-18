ba2f6ec31b4e7d50098d33994ea5cf685f85630e
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-18 11:48:49 -0200

    Refatoração na classe Imagem para usar herança

--- image_binarizator.rb
+++ image_binarizator.rb
@@ -10,27 +10,22 @@ threshold deve variar entre 0 e 1
 
 pixel.red   imprime o valor do pixel vermelho
 pixel.blue  imprime o valor do pixel azul
-pixel.green imprime o valor do pixel
+pixel.green imprime o valor do pixel verde
 
 =end
 
-class Imagem < Magick::Image
-
-  def initialize(nome_da_imagem)
-    @nome_do_arquivo, @extensao_do_arquivo = nome_da_imagem.split('.')
-    @imagem = ImageList.new("#{nome_da_imagem}")
-  end
-
-  def converter_imagem_para_tons_de_cinza
-    @imagem = @imagem.quantize(256, Magick::GRAYColorspace)
+class Imagem < Magick::ImageList
+  
+	def converter_imagem_para_tons_de_cinza
+    self.quantize(256, Magick::GRAYColorspace)
   end
 
   def aplicar_threshold(valor_do_threshold)
     valor_do_threshold *= 65535
-    for x in 0..@imagem.columns-1
-      for y in 0..@imagem.rows-1
-        (@imagem.pixel_color(x,y).red >= valor_do_threshold ? 
-          @imagem.pixel_color(x,y,"black") : @imagem.pixel_color(x,y,"white"))
+    for x in 0..self.columns-1
+      for y in 0..self.rows-1
+        (self.pixel_color(x,y).red >= valor_do_threshold ? 
+          self.pixel_color(x,y,"black") : self.pixel_color(x,y,"white"))
       end
     end
   end
@@ -39,22 +34,26 @@ class Imagem < Magick::Image
     imagem.write("#{nome_do_arquivo}")
   end
 
-  def binarizar_imagem(threshold=1)
-    converter_imagem_para_tons_de_cinza
-    aplicar_threshold(threshold)
-    gravar_imagem(@imagem, 
-      "#{@nome_do_arquivo}_versao_binarizada.#{@extensao_do_arquivo}")
+  def extrair_nome_da_imagem
+  	@nome_da_imagem, @extensao_da_imagem = self.filename.split('.')
   end
 
-  private :converter_imagem_para_tons_de_cinza, :gravar_imagem, :aplicar_threshold
+  def binarizar_imagem(threshold=1)
+    extrair_nome_da_imagem
+    converter_imagem_para_tons_de_cinza
+    aplicar_threshold(threshold)
+    gravar_imagem(self, 
+      "#{@nome_da_imagem}_binarizada.#{@extensao_da_imagem}")
+  end
+
+  private :converter_imagem_para_tons_de_cinza, :gravar_imagem, :aplicar_threshold, :extrair_nome_da_imagem
 
 end
 
 
+#c = Imagem.new("bridge.jpg")
+#c.binarizar_imagem 0.5
+#c = Teste.new("bridge.jpg")
 c = Imagem.new("bridge.jpg")
 c.binarizar_imagem 0.5
-
-
-
-
 
