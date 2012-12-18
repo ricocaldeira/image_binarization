64b9ba2ffa086b7a347ba8843deec2578abf8af3
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-17 23:56:17 -0200

    Refatoração de métodos e definição de método privado

--- image_binarizator.rb
+++ image_binarizator.rb
@@ -17,7 +17,7 @@ pixel.green imprime o valor do pixel
 class Imagem
 
 	def initialize(nome_da_imagem)
-		@nome_do_arquivo = nome_da_imagem.sub(".jpg", "")
+		@nome_do_arquivo, @extensao_do_arquivo = nome_da_imagem.split('.')
 		@imagem = ImageList.new("#{nome_da_imagem}")
 	end
 
@@ -29,39 +29,30 @@ class Imagem
 		valor_do_threshold *= 65535
 		for x in 0..@imagem.columns-1
 			for y in 0..@imagem.rows-1
-				if @imagem.pixel_color(x,y).red >= valor_do_threshold
-					@imagem.pixel_color(x,y,"black")
-				else
-					@imagem.pixel_color(x,y,"white")
-				end
+				(@imagem.pixel_color(x,y).red >= valor_do_threshold ? 
+					@imagem.pixel_color(x,y,"black") : @imagem.pixel_color(x,y,"white"))
 			end
 		end
-	end
-
-	def mostra_pixels
-		@imagem.each_pixel do |pixel|
-			puts pixel
-		end
-	end
-
-	def binarizar_imagem(threshold=1)
-		converter_imagem_para_tons_de_cinza
-		aplicar_threshold(threshold)
-		gravar_imagem(@imagem, "#{@nome_do_arquivo}_versao_binarizada.jpg")
 	end
 
 	def gravar_imagem(imagem, nome_do_arquivo)
 		imagem.write("#{nome_do_arquivo}")
 	end
 
-	private :converter_imagem_para_tons_de_cinza, :gravar_imagem
+	def binarizar_imagem(threshold=1)
+		converter_imagem_para_tons_de_cinza
+		aplicar_threshold(threshold)
+		gravar_imagem(@imagem, 
+			"#{@nome_do_arquivo}_versao_binarizada.#{@extensao_do_arquivo}")
+	end
+
+	private :converter_imagem_para_tons_de_cinza, :gravar_imagem, :aplicar_threshold
 
 end
 
 
 c = Imagem.new("bridge.jpg")
 c.binarizar_imagem 0.5
-
 
 
 
