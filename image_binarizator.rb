a79aa9151d75101ee6f936eb5b385f3a19748df3
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-18 00:09:22 -0200

    Substituindo tabs por espaços

--- image_binarizator.rb
+++ image_binarizator.rb
@@ -14,39 +14,39 @@ pixel.green imprime o valor do pixel
 
 =end
 
-class Imagem
+class Imagem < Magick::Image
 
-	def initialize(nome_da_imagem)
-		@nome_do_arquivo, @extensao_do_arquivo = nome_da_imagem.split('.')
-		@imagem = ImageList.new("#{nome_da_imagem}")
-	end
+  def initialize(nome_da_imagem)
+    @nome_do_arquivo, @extensao_do_arquivo = nome_da_imagem.split('.')
+    @imagem = ImageList.new("#{nome_da_imagem}")
+  end
 
-	def converter_imagem_para_tons_de_cinza
-		@imagem = @imagem.quantize(256, Magick::GRAYColorspace)
-	end
+  def converter_imagem_para_tons_de_cinza
+    @imagem = @imagem.quantize(256, Magick::GRAYColorspace)
+  end
 
-	def aplicar_threshold(valor_do_threshold)
-		valor_do_threshold *= 65535
-		for x in 0..@imagem.columns-1
-			for y in 0..@imagem.rows-1
-				(@imagem.pixel_color(x,y).red >= valor_do_threshold ? 
-					@imagem.pixel_color(x,y,"black") : @imagem.pixel_color(x,y,"white"))
-			end
-		end
-	end
+  def aplicar_threshold(valor_do_threshold)
+    valor_do_threshold *= 65535
+    for x in 0..@imagem.columns-1
+      for y in 0..@imagem.rows-1
+        (@imagem.pixel_color(x,y).red >= valor_do_threshold ? 
+          @imagem.pixel_color(x,y,"black") : @imagem.pixel_color(x,y,"white"))
+      end
+    end
+  end
 
-	def gravar_imagem(imagem, nome_do_arquivo)
-		imagem.write("#{nome_do_arquivo}")
-	end
+  def gravar_imagem(imagem, nome_do_arquivo)
+    imagem.write("#{nome_do_arquivo}")
+  end
 
-	def binarizar_imagem(threshold=1)
-		converter_imagem_para_tons_de_cinza
-		aplicar_threshold(threshold)
-		gravar_imagem(@imagem, 
-			"#{@nome_do_arquivo}_versao_binarizada.#{@extensao_do_arquivo}")
-	end
+  def binarizar_imagem(threshold=1)
+    converter_imagem_para_tons_de_cinza
+    aplicar_threshold(threshold)
+    gravar_imagem(@imagem, 
+      "#{@nome_do_arquivo}_versao_binarizada.#{@extensao_do_arquivo}")
+  end
 
-	private :converter_imagem_para_tons_de_cinza, :gravar_imagem, :aplicar_threshold
+  private :converter_imagem_para_tons_de_cinza, :gravar_imagem, :aplicar_threshold
 
 end
 
