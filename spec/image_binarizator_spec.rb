580c7a7cbcbff6feca816f4fbd4e6a5c1ddc96f5
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-12-18 12:49:43 -0200

    Adicionados testes para a classe Imagem

---
+++ spec/image_binarizator_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../image_binarizator'
+require 'spec_helper'
+
+describe Imagem do
+	it { respond_to :binarizar_imagem }
+
+  context "#binarizar_imagem" do
+    before :each do
+      @img = Imagem.new("bridge.jpg")
+      @img.binarizar_imagem
+    end
+
+    it "Image pixels has the same values" do
+      @img.each_pixel do |pixel|
+        (pixel.red == pixel.blue and pixel.red == pixel.green).should be_true
+      end
+    end
+
+    it "Image pixels color are black or white" do
+      @img.each_pixel do |pixel|
+        ([0, 65535].include? pixel.red).should be_true
+      end
+    end
+
+  end  
+end
